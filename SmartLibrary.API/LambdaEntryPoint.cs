71d36699440aed57ddffff4f41a97150a6c9c9c4
Gutovp1 <paivaav@gmail.com>
2022-12-14 17:36:48 -0300

    Include amazon package and adapt connection string. Add extension method webhost

---
+++ SmartLibrary.API/LambdaEntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.Lambda.AspNetCoreServer;
+
+namespace SmartLibrary.API
+{
+    public class LambdaEntryPoint : APIGatewayHttpApiV2ProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            //base.Init(builder);
+            builder.UseStartup<Startup>();
+        }
+    }
+}
